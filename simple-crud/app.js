df22400593490616be5d9b038045a7d8b9d7c7e5
Raven <ravencarlostrodrigo@gmail.com>
2021-10-09 14:54:02 +0800

    Add click event listener for add button

--- simple-crud/app.js
+++ simple-crud/app.js
@@ -2,3 +2,6 @@ let inputTodo = document.getElementById('todo-text');
 let submitTodo = document.getElementById('submit-todo');
 let containerTodo = document.getElementById('todo-container');
 
+submitTodo.addEventListener('click', function(){
+    
+});
